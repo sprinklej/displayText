9e0bf2e062b655fab685ab5e832785ca2ca7b445
Joel <joel.sprinkle@carleton.ca>
2015-11-30 19:38:33 -0500

    added relative filepath

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "displayteamscontroller.h"
 #include <QApplication>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    QDir current = QDir::current();
+    current.cdUp();
+    QString summaryPath = current.path() + "/DisplayText/summary.txt";
+
     DisplayTeamsController disTeamsCon;
-    disTeamsCon.showReport("/home/admin/DisplayText/summary.txt", 0); //summary report
+    disTeamsCon.showReport(summaryPath, 0); //summary report
     disTeamsCon.saveToFile(); //Detailed report
 
     return a.exec();
